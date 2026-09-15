9321c1be7eee3ae65e487d67cdbef2687641c5bf
unknown <you@example.com>
2026-09-15 10:56:53 +0500

    solved

--- intro/time_seconds/time_seconds.py
+++ intro/time_seconds/time_seconds.py
@@ -1,2 +1,2 @@
 seconds=int(input())
-print(f"{seconds} second(s) = {seconds//60} minite(s) and {seconds%60} second(s)")
+print(f"{seconds} second(s) = {seconds//60} minute(s) and {seconds%60} second(s)")
