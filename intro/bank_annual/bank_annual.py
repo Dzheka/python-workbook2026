1d894bbd6b53530b17059df881b9b1c38e31631e
Firdavs601 <rakhmanovfirdavs47@gmail.com>
2026-09-15 10:33:18 +0500

    solved 10+ problems

--- intro/bank_annual/bank_annual.py
+++ intro/bank_annual/bank_annual.py
@@ -1,7 +1,7 @@
 initial_deposit=float(input("Enter the initial deposit: ".strip()))
 interest_rate=float(input("Enter the annual interest rate (%): ").strip())   
 years=int(input("Enter the number of years: ").strip())
-rate_coefficient= interest_rate/100
+rate_coefficient= interest_rate/1200
 final_balance= initial_deposit*(1+rate_coefficient)**years
 if years==1:
     print(f"Balance after {years} years: {final_balance:.2f}")
